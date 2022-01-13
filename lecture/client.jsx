295a78c741a938d681fecb3228ff6ec3a4f05e61
dmstjd3256 <dmstjd3256@naver.com>
2022-01-14 01:26:56 +0900

    lotto game

--- lecture/client.jsx
+++ lecture/client.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDom from 'react-dom' // 노드에 설치된 것을 불러오는 것
-import RSP from './RSP'
+import Lotto from './lotto'
 
-ReactDom.render(<RSP />, document.querySelector('#root'))
+ReactDom.render(<Lotto />, document.querySelector('#root'))
